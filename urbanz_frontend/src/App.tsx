858b521f9429a965582ee940a8cb5e9745cb2a9a
Noah8820 <noah.deboni@noseryoung.com>
2023-04-24 14:58:45 +0200

    updating frontend and backend project

--- urbanz_frontend/src/App.tsx
+++ urbanz_frontend/src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {data}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
