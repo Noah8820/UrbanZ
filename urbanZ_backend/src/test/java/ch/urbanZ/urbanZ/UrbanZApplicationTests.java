af1f7f5857e0866fbd8a31d75f0979a9e980d74f
Noah8820 <noah.deboni@noseryoung.com>
2023-04-24 13:35:25 +0200

    renaiming backend project

---
+++ urbanZ_backend/src/test/java/ch/urbanZ/urbanZ/UrbanZApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.urbanZ.urbanZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
